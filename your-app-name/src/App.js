6e16f9f24337fac2ecfd59dee2a1ebab8d74cb1e
Madeleine <madeleinejsong@gmail.com>
2024-10-31 16:41:31 -0400

    colors and grid workable

--- your-app-name/src/App.js
+++ your-app-name/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      {[...Array(30)].map((_, index) => {
+      {[...Array(24)].map((_, index) => {
         if (index === 0) { // top-left diagonal position
           return (
             <div key={index} className="square">
@@ -34,11 +34,20 @@ function App() {
               </video>
             </div>
           );
-        } else if (index === 29) { // bottom-right diagonal position
+        } else if (index === 10) { // bottom-right diagonal position
           return (
             <div key={index} className="square">
               <video ref={videoRefs[2]} className="video" autoPlay loop muted>
                 <source src={`${process.env.PUBLIC_URL}/video3.mp4`} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          );
+        } else if (index === 22) { // bottom-right diagonal position
+          return (
+            <div key={index} className="square">
+              <video ref={videoRefs[2]} className="video" autoPlay loop muted>
+                <source src={`${process.env.PUBLIC_URL}/exerciseroom.mp4`} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
